c3c498db24f5fd9878b59bff7739909f4fab5745
madhatter <madhatter@madhatters-MacBook-Air.local>
2015-05-06 22:40:17 -0700

    challenge 5 working

--- challenge_5.py
+++ challenge_5.py
@@ -9,22 +9,25 @@ counter = 0
 newvar = 0
 parameter = {'nothing':'12345'}
 params = urllib.urlencode(parameter)
-pattern = re.compile('and the next nothing [0-9].*')
+pattern = re.compile('and the next nothing is [0-9].*')
+new_pattern = re.compile('[0-9].*')
 
 while counter < 400:
 	result = urllib.urlopen("http://www.pythonchallenge.com/pc/def/linkedlist.php?%s" % params)
 	inquery = result.read()
 	print "Page: \n%s" % inquery
 	stringed = re.findall(pattern, inquery)
+	new_stringed = re.findall(new_pattern, stringed[0])
+	print stringed
 	print stringed[0]
-	if stringed[0] == '16044':
-		stringed[0] = '8022'
-		newvar = stringed[0]
-	elif stringed[0] == '82683':
-		stringed[0] = '63579'
-		newvar = stringed[0]
+	if new_stringed[0] == '16044':
+		new_stringed[0] = '8022'
+		newvar = new_stringed[0]
+	#elif stringed[0] == '82683':
+	#	stringed[0] = '63579'
+	#	newvar = new_stringed[0]
 	else:
-		newvar = stringed[0]
+		newvar = new_stringed[0]
 	params = urllib.urlencode({'nothing':newvar})
 	parameter['nothing'] = newvar
 	counter += 1
